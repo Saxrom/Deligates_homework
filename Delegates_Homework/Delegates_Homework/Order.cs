efc78dffcb7491cc1fa7e08d07c112f948c065b3
Shahrom <shahromkr@code.edu.az>
2022-05-18 17:49:23 +0400

    Added homework about deligates

---
+++ Delegates_Homework/Delegates_Homework/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Delegates_Homework
+{
+    public class Order
+    {
+        Products[] Products;
+        public double TotalPrice;
+        DateTime date;
+
+        public Order(Products[] product,double totalPrice)
+        {
+            TotalPrice = totalPrice;
+            Products = product;
+        }
+    }
+}
---
+++ Delegates_Homework/Delegates_Homework/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Delegates_Homework
+{
+    public class Products
+    {
+        public string Name;
+        public double Price;
+        public static byte Count;
+
+        static Products()
+        {
+            Count = 0;
+        }
+
+        public Products(string name,double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
